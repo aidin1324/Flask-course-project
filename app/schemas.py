695db621a427f2da51eab02901b04dc654bbd917
Aidin <aidinror@gmail.com>
2024-06-12 16:53:12 +0600

    -added databases and working with db

--- app/schemas.py
+++ app/schemas.py
@@ -2,20 +2,25 @@ from marshmallow import schema, fields, Schema
 
 
 class PlainItemSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(precision=2,required=True)
 
 
 class PlainStoreSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
 
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float(precision=2)
     store_id = fields.Str()
+
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
@@ -24,3 +29,9 @@ class ItemSchema(PlainItemSchema):
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
